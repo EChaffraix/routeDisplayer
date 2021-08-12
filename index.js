6b4821657bc281d91ec8d1942724e3ede571424a
Emmanuel CHAFFRAIX <echaffraix@talentsoft.com>
2021-08-12 11:50:59 +0200

    Add Dockerfile

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 8080
 
 app.get('/*', (req, res) => {
-    console.log(`Call on : ${req.originalUrl} -> ${req.url}`);
+    console.log(`${process.env.NAME}|Call on : ${req.originalUrl} -> ${req.url}`);
     res.send(`Call received on : ${req.originalUrl} -> ${req.url}`);
 })
 
